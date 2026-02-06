2874d2e9acd65321060fd1119d64c3b01ab0748e
Ray1358 <ramonbaez1358@gmail.com>
2026-02-05 21:38:30 -0500

    Initial Spring Boot project setup

---
+++ src/test/java/com/ramonbaez/inventoryordersapi/InventoryOrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonbaez.inventoryordersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryOrdersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
